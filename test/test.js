930bef2ebb4e18393b4530f20ecf39eb24199827
swart\Govert <gdswa@outlook.com>
2017-10-05 09:43:50 +0200

    x

--- test/test.js
+++ test/test.js
@@ -5,10 +5,32 @@
 var supertest = require('supertest');
 
 var server = supertest.agent("http://localhost:3000/");
-
+/**
+ * Good weather for getting api/persons
+ */
 describe("Users unittest", function () {
-it("Should return a json file", function (done) {
-    server.get("api/persons").expect("Content-type", /json/).expect(200,done);
+    it("Should return a json file", function (done) {
+        server.get("api/persons").expect("Content-type", /json/).expect(200,done);
+    });
 });
+
+
+/**
+ * Bad weather for empty post to /api/persons/
+ */
+describe("Post an empty body to the users", function () {
+    it("Should return a html file", function (done) {
+        server.post("api/persons").expect("Content-type", /text/);
+    });
+});
+
+describe("Post a correct body to the users", function () {
+    it("Should return a json file and a 200 code", function (done) {
+        server.post("api/persons").send("{"name": "test",
+            "lastname": "testo",
+            "username": "testman",
+            "password": "tester""}
+        ).expect("Content-type", /json/).expect(200,done);
+    });
 });
 
